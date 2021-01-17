7e370e4e5a3b272a10125642c22720b2b556430b
Selena <selenazhxng@gmail.com>
2021-01-16 20:45:11 -0800

    import Link

--- client/src/components/modules/HomeButton.js
+++ client/src/components/modules/HomeButton.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import { Link } from "@reach/router";
 import "./HomeButton.css";
 
 class HomeButton extends Component {
