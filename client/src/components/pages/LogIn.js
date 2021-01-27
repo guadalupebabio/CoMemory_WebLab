ce9773cb60eb965fc188b2c47f8322e1d4758e67
Selena <selenazhxng@gmail.com>
2021-01-27 01:41:44 -0800

    redirect from login if user is logged in

--- client/src/components/pages/LogIn.js
+++ client/src/components/pages/LogIn.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import GoogleLogin, { GoogleLogout } from "react-google-login";
 import RegLine from "../modules/RegLine";
 import WhiteButton from "../modules/WhiteButton.js";
+import { navigate } from '@reach/router';
 
 //import "../../utilities.css";
 import "./LogIn.css";
@@ -22,31 +23,36 @@ class LogIn extends Component {
   }
 
   render() {
-    return (
-      <>
-      <div id="Registration-container" className="registration">
-        <h1>Log In</h1>
-        {/* TODO: fix action attribute */}
-          <form action="..." method="post"> 
-            <ul>
-              <li>
-                <RegLine typeValue="string" idValue="username" placeholderValue="username" />
-              </li>
-              <li>
-                <RegLine typeValue="password" idValue="password" placeholderValue="password" />
-              </li>
-              <li>
-              <WhiteButton text="Log in" linkDestination="/contributestep1" />  
-              </li>
-              <p id="smalltxt">Don't have an account? <b><a href="/register">Sign up</a></b></p>
-
-            </ul>
-          </form>
-          
-      </div>
-    
-      </>
-    );
+    if (this.props.userId) {
+      navigate('/personalspace');
+      return null;
+    } else {
+      return (
+        <>
+        <div id="Registration-container" className="registration">
+          <h1>Log In</h1>
+          {/* TODO: fix action attribute */}
+            <form action="..." method="post"> 
+              <ul>
+                <li>
+                  <RegLine typeValue="string" idValue="username" placeholderValue="username" />
+                </li>
+                <li>
+                  <RegLine typeValue="password" idValue="password" placeholderValue="password" />
+                </li>
+                <li>
+                <WhiteButton text="Log in" linkDestination="/contributestep1" />  
+                </li>
+                <p id="smalltxt">Don't have an account? <b><a href="/register">Sign up</a></b></p>
+  
+              </ul>
+            </form>
+            
+        </div>
+      
+        </>
+      );
+    }
   }
 }
 
--- client/src/components/pages/Registration.js
+++ client/src/components/pages/Registration.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import GoogleLogin, { GoogleLogout } from "react-google-login";
 import RegLine from "../modules/RegLine";
 import WhiteButton from "../modules/WhiteButton.js";
+import { navigate } from '@reach/router';
 
 //import "../../utilities.css";
 import "./Registration.css";
@@ -22,51 +23,56 @@ class Registration extends Component {
   }
 
   render() {
-    return (
-      <>
-      <div id="Registration-container" className="registration">
-        <h1>Register</h1>
-        {/* TODO: fix action attribute */}
-          <form action="..." method="post"> 
-            <ul>
-              <li>
-                <RegLine typeValue="string" idValue="email" placeholderValue="somebody@example.com" />
-              </li>
-              <li>
-                <RegLine typeValue="string" idValue="username" placeholderValue="username" />
-              </li>
-              <li>
-                <RegLine typeValue="password" idValue="password" placeholderValue="password" />
-              </li>
-              <li>
-              <WhiteButton text="Sign up" linkDestination="/contributestep1" />  
-              </li>
-              <li>
-                {this.props.userId ? (
-                  <GoogleLogout
-                    clientId={GOOGLE_CLIENT_ID}
-                    buttonText="Logout"
-                    onLogoutSuccess={this.props.handleLogout}
-                    onFailure={(err) => console.log(err)}
-                  />
-                ) : (
-                  <GoogleLogin
-                    clientId={GOOGLE_CLIENT_ID}
-                    buttonText="Log in with Google"
-                    onSuccess={this.props.handleLogin}
-                    onFailure={(err) => console.log(err)}
-                  />
-                )}
-                <p id="smalltxt">already have an account? <b><a href="/login">Log in</a></b></p>
-              </li>
-            </ul>
-          </form>
-          
-      </div>
-      
-       
-      </>
-    );
+    if (this.props.userId) {
+      navigate('/personalspace');
+      return null;
+    } else {
+      return (
+        <>
+        <div id="Registration-container" className="registration">
+          <h1>Register</h1>
+          {/* TODO: fix action attribute */}
+            <form action="..." method="post"> 
+              <ul>
+                <li>
+                  <RegLine typeValue="string" idValue="email" placeholderValue="somebody@example.com" />
+                </li>
+                <li>
+                  <RegLine typeValue="string" idValue="username" placeholderValue="username" />
+                </li>
+                <li>
+                  <RegLine typeValue="password" idValue="password" placeholderValue="password" />
+                </li>
+                <li>
+                <WhiteButton text="Sign up" linkDestination="/contributestep1" />  
+                </li>
+                <li>
+                  {this.props.userId ? (
+                    <GoogleLogout
+                      clientId={GOOGLE_CLIENT_ID}
+                      buttonText="Logout"
+                      onLogoutSuccess={this.props.handleLogout}
+                      onFailure={(err) => console.log(err)}
+                    />
+                  ) : (
+                    <GoogleLogin
+                      clientId={GOOGLE_CLIENT_ID}
+                      buttonText="Log in with Google"
+                      onSuccess={this.props.handleLogin}
+                      onFailure={(err) => console.log(err)}
+                    />
+                  )}
+                  <p id="smalltxt">already have an account? <b><a href="/login">Log in</a></b></p>
+                </li>
+              </ul>
+            </form>
+            
+        </div>
+        
+         
+        </>
+      );
+    }
   }
 }
 
