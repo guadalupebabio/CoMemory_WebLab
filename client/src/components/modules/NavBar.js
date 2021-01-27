7225ca3de108a5fc21773ac5557a015cb3f8c0f5
Selena <selenazhxng@gmail.com>
2021-01-27 02:15:43 -0800

    display login/logout in nav based on login status

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -1,30 +1,45 @@
-import React, { Component } from "react";
-import GoogleLogin, { GoogleLogout } from "react-google-login";
+import React, { Component } from 'react';
+import GoogleLogin, { GoogleLogout } from 'react-google-login';
 import WhiteButton from '../modules/WhiteButton.js';
 
-import "./NavBar.css";
+import './NavBar.css';
 
-const GOOGLE_CLIENT_ID = "136526920473-t1lo12n7ojqbh3um8t84j3jdbc07i64n.apps.googleusercontent.com";
+/**
+ * 
+ * Proptypes
+ * @param {string} userId
+ * 
+ */
 
+const GOOGLE_CLIENT_ID = '136526920473-t1lo12n7ojqbh3um8t84j3jdbc07i64n.apps.googleusercontent.com';
 
 class NavBar extends Component {
-    constructor(props) {
-        super(props);
-    }
+	constructor(props) {
+		super(props);
+	}
 
-    render() {
-        return (
-            <div className="upperright">
-						<WhiteButton text="New" linkDestination="/contributestep1" />
-                        <GoogleLogout
-                      clientId={GOOGLE_CLIENT_ID}
-                      buttonText="Logout"
-                      onLogoutSuccess={this.props.handleLogout}
-                      onFailure={(err) => console.log(err)}
-                    />
+	render() {
+		return (
+			<div className="upperright">
+				<WhiteButton text="New" linkDestination="/contributestep1" />
+				{this.props.userId ? (
+					<GoogleLogout
+						clientId={GOOGLE_CLIENT_ID}
+						buttonText="Logout"
+						onLogoutSuccess={this.props.handleLogout}
+						onFailure={(err) => console.log(err)}
+					/>
+				) : (
+					<GoogleLogin
+						clientId={GOOGLE_CLIENT_ID}
+						buttonText="Log in with Google"
+						onSuccess={this.props.handleLogin}
+						onFailure={(err) => console.log(err)}
+					/>
+				)}
 			</div>
-        );
-    }
+		);
+	}
 }
 
 export default NavBar;
