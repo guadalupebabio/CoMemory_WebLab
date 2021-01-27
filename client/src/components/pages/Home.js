6ca28a57c8a2b7c9ca7b3ccf0d38146f0b96a76b
Selena <selenazhxng@gmail.com>
2021-01-27 08:20:53 -0800

    clean code

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -34,7 +34,6 @@ class Home extends Component {
                 <p id="subtitle" >Grieving has changed. Approximately 420,000 Americans have died from the coronavirus, and more than 2 million people in the world have succumbed to the disease.  Let’s leverage the digital realm that keep us together to host the platform that will allow our community to commemorate those who are no longer with us.</p>
                 </div>
                 <HomeButton text="EXPLORE THE GRIEVING SPACE" linkDestination="/grievingspace" />
-                {/* <HomeButton text="MAKE A CONTRIBUTION" linkDestination={goTo} /> */}
                 <HomeButton text="MAKE A CONTRIBUTION" linkDestination="/register" />
                 </div>
                 
