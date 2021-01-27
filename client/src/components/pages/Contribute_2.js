4678280e933347f5ea2d16ef0364fb299d3f34f1
Selena <selenazhxng@gmail.com>
2021-01-27 00:50:52 -0800

    restrict Contribute access to logged in users

--- client/src/components/pages/Contribute_2.js
+++ client/src/components/pages/Contribute_2.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import WhiteButton from '../modules/WhiteButton.js';
 import DownloadButton from '../modules/DownloadButton.js';
 import P5Wrapper from 'react-p5-wrapper';
-import ReactDOM from 'react-dom';
 import sketch from '../modules/sketch';
 import Board from '../modules/Board';
 
 import '../../utilities.css';
 import './Contribute_2.css';
-import "../modules/Board.css";
+import '../modules/Board.css';
 
-import { get, post } from '../../utilities';
+import { get } from '../../utilities';
 
 /**
  * 
  * Proptypes
+ * @param {string} userId
  * @param {string} recentBoardId the id of the most recently uploaded board
  * @param {function} updateBoard updates which board has been uploaded most recently
  * 
@@ -24,14 +24,14 @@ class Contribute_2 extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-            username: '',
-            board_id: '',
+			username: '',
+			board_id: '',
 			boards: []
 		};
 	}
 
 	componentDidMount() {
-        document.title = "Your Memory";
+		document.title = 'Your Memory';
 		get('/api/whoami').then((user) => {
 			if (user.name) {
 				// they are registed in the database, and currently logged in.
@@ -48,39 +48,53 @@ class Contribute_2 extends Component {
 	}
 
 	render() {
-		let boardList = null;
-		const hasBoards = this.state.boards.length !== 0;
-		if (hasBoards) {
-			boardList = this.state.boards.map((boardObj) => (
-				<Board
-					key={`Board_${boardObj._id}`}
-					_id={boardObj._id}
-					creator_id={boardObj.creator_id}
-					honoree_name={boardObj.honoree_name}
-					date={boardObj.date}
-					place={boardObj.place}
-					msg={boardObj.msg}
-				/>
-			));
-		} else {
-			boardList = <div className="Board-styles">Create a memory</div>;
-		}
-		const state = { rotation: 160 };
-		return (
-			<div className="flowercontainer">
-                {boardList[boardList.length - 1]}
-				<div className="cont2flower">
-					<P5Wrapper sketch={(p) => {return sketch(p, this.state.board_id)}} shape={this.state.shape} />
-				</div>
-				{/* <input type="range" defaultValue={state.shape} min="0" max="360" step="1" onChange={event => setState({ ...state, shape: event.target.value })}/> */}
+		if (this.props.userId) {
+			let boardList = null;
+			const hasBoards = this.state.boards.length !== 0;
+			if (hasBoards) {
+				boardList = this.state.boards.map((boardObj) => (
+					<Board
+						key={`Board_${boardObj._id}`}
+						_id={boardObj._id}
+						creator_id={boardObj.creator_id}
+						honoree_name={boardObj.honoree_name}
+						date={boardObj.date}
+						place={boardObj.place}
+						msg={boardObj.msg}
+					/>
+				));
+			} else {
+				boardList = <div className="Board-styles">Create a memory</div>;
+			}
+			const state = { rotation: 160 };
+			return (
+				<div className="flowercontainer">
+					{boardList[boardList.length - 1]}
+					<div className="cont2flower">
+						<P5Wrapper
+							sketch={(p) => {
+								return sketch(p, this.state.board_id);
+							}}
+							shape={this.state.shape}
+						/>
+					</div>
+					{/* <input type="range" defaultValue={state.shape} min="0" max="360" step="1" onChange={event => setState({ ...state, shape: event.target.value })}/> */}
 
-				<div className="cont2buttons">
-					<a href="/contributestep1">back</a>
-					<DownloadButton text="Download" linkDestination="/" />
-					<WhiteButton text="Submit" linkDestination="/personalspace" />
+					<div className="cont2buttons">
+						<a href="/contributestep1">back</a>
+						<DownloadButton text="Download" linkDestination="/" />
+						<WhiteButton text="Submit" linkDestination="/personalspace" />
+					</div>
 				</div>
-			</div>
-		);
+			);
+		} else {
+			return (
+                <div className="u-flex u-flex-justifyCenter u-flex-alignCenter">
+                    <div>Please log in to contribute!</div>
+                    <WhiteButton text="Login" linkDestination="/register" />
+                </div>
+            );
+		}
 	}
 }
 
