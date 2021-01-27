3a2e8b4d4d6c1e6523ecb9610032b8d55bea76ee
GBabio <guadalupe.babio@gmail.com>
2021-01-27 12:59:08 -0500

    delete button

--- client/src/components/pages/PersonalSpace.js
+++ client/src/components/pages/PersonalSpace.js
@@ -69,7 +69,7 @@ class PersonalSpace extends Component {
 			} else {
 				boardList = [
 					<div className="Board-styles">Create a memory.</div>,
-					<WhiteButton text="New" linkDestination="/contributestep1" />
+					
 				];
 			}
 
