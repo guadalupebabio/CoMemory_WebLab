29f5bba080894809d876fa771a8c79cf6a2ce43b
Selena <selenazhxng@gmail.com>
2021-01-27 02:32:04 -0800

    add Nav; add get flowers

--- client/src/components/pages/GrievingSpace.js
+++ client/src/components/pages/GrievingSpace.js
@@ -1,34 +1,44 @@
 import React, { Component } from "react";
-import WhiteButton from "../modules/WhiteButton.js";
+import WhiteButton from "../modules/WhiteButton";
+import NavBar from "../modules/NavBar";
 
 import "../../utilities.css";
 import "./GrievingSpace.css";
+import { get } from "../../utilities.js";
+
+/**
+ * 
+ * Proptypes
+ * @param {string} userId
+ * @param {function} handleLogin
+ * @param {function} handleLogout
+ * 
+ */
 
 class GrievingSpace extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: String,
-            isLoggedIn: false
+            username: "",
+            flowers: [],
         }
     }
 
     componentDidMount() {
-        
+        get('/api/getImages').then((flowers) => {
+            console.log('got flowers');
+            this.setState({
+                flowers,
+            });
+		});
     }
 
     render() {
         return (
             <div id="" className="">
-                <p id="header">A virtual space to honor and grieve  loved ones</p>
-                <div class="upperright">
-                {/* <WhiteButton text="New" linkDestination="/contributestep1" /> */}
-                {this.state.isLoggedIn ? (
-                    <WhiteButton text="New" linkDestination="/contributestep1" />
-                  ):(
-                    <WhiteButton text="New" linkDestination="/login" />
-                  )}
-                </div>
+                <NavBar userId={this.props.userId} handleLogin={this.props.handleLogin} handleLogout={this.props.handleLogout} />
+                {this.state.flowers.length === 0 ? "Contribute a memory" : this.state.flowers.map((flower) => (<img src={flower}></img>))}
+                <p id="header">A virtual space to honor and grieve loved ones</p>
                 <p id="footer">Hover over image to visualize text</p>
             </div>
         )
